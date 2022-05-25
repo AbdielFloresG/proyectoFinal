e137717f344bc31bde2ae9f7460778929d22d7df
Abdiel Flores <abdielfg@gmail.com>
2022-05-24 19:12:33 -0600

    Cambio a php el index

--- index.php
+++ index.php
@@ -11,9 +11,7 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <header class="container-fluid d-flex justify-content-center">
-        <p class="text-light mb-0"> Hola perro</p>
-    </header>
+
     <nav class="navbar navbar-expand-lg bg-black">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Navbar</a>
@@ -23,7 +21,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home Abdiel </a>
+                <a class="nav-link active" aria-current="page" href="index.html">Inicio </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
