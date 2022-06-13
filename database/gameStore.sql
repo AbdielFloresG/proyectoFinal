f083f6c1d3615ea7bddc9811f34ae8be746e41bb
Abdiel Flores <abdielfg@gmail.com>
2022-06-13 02:55:09 -0600

    Actualizacion de la base de datos

--- database/gameStore.sql
+++ database/gameStore.sql
@@ -13,10 +13,27 @@ idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nombreUsuario VARCHAR(50) NOT NULL,
 apellidoUsuario VARCHAR(50) NOT NULL,  
 correoUsuario VARCHAR(50) NOT NULL UNIQUE,  
-passwordUsuario VARCHAR(250) NOT NULL,
+passwordUsuario VARCHAR(1000) NOT NULL,
 rolUsuario ENUM('user','admin') DEFAULT 'user' NOT NULL
 );
 
+CREATE TABLE Juego(
+idJuego INT PRIMARY KEY AUTO_INCREMENT, 
+nombreJuego VARCHAR(50) NOT NULL, 
+precio FLOAT NOT NULL,
+desarrollador VARCHAR(50) NOT NULL,
+genero VARCHAR(100) NOT NULL,
+fechaLanzamiento INT NOT NULL,
+descripcion VARCHAR(1000) NOT NULL,
+activo INT NOT NULL
+);
+
+CREATE TABLE Venta(
+idVenta INT PRIMARY KEY AUTO_INCREMENT,
+idUsuario INT, FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario) ON UPDATE CASCADE ON DELETE CASCADE,
+fechaVenta DATETIME,
+monto FLOAT NOT NULL
+);
 
 CREATE TABLE detalleVenta(
 	idTrasaccion INT PRIMARY KEY AUTO_INCREMENT,
@@ -27,27 +44,10 @@ CREATE TABLE detalleVenta(
 );
 
 
-CREATE TABLE Venta(
-idVenta INT PRIMARY KEY AUTO_INCREMENT,
-idUsuario INT, FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario) ON UPDATE CASCADE ON DELETE CASCADE,
-fechaVenta DATETIME,
-monto FLOAT NOT NULL
-);
-
-CREATE TABLE Venta(
-idVenta INT PRIMARY KEY AUTO_INCREMENT,
-idUsuario INT, FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario) ON UPDATE CASCADE ON DELETE CASCADE,
-idJuego INT, FOREIGN KEY(idJuego) REFERENCES Juego(idJuego) ON UPDATE CASCADE ON DELETE CASCADE,
-cantidad INT NOT NULL,
-monto FLOAT NOT NULL,
-fechaVenta DATE NOT NULL
-);
 
 
-
-INSERT INTO `gamestore`.`usuario` (`nombreUsuario`, `apellidoUsuario`, `correoUsuario`, `passwordUsuario`,`rolUsuario`) VALUES ('Abdiel', 'Flores', 'admin', '123456','admin');
+INSERT INTO `gamestore`.`usuario` (`nombreUsuario`, `apellidoUsuario`, `correoUsuario`, `passwordUsuario`,`rolUsuario`) VALUES ('Abdiel', 'Flores', 'admin', 'ef8eb12674e8e1358266c00f9380fe1be78c07dc','admin');
 INSERT INTO `gamestore`.`usuario` (`nombreUsuario`, `apellidoUsuario`, `correoUsuario`, `passwordUsuario`,`rolUsuario`) VALUES ('Abdiel', 'Flores', 'a', '123456','user');
-
 
 INSERT INTO juego VALUES (0, 'Rainbow Six Siege', 200.00, 'Ubisoft', 'Shooter', 2015,'Videojuego de disparos táctico en línea desarrollado por Ubisoft Montreal y distribuidor por Ubisoft.', 1);
 INSERT INTO juego VALUES (0, 'GTA V', 300.00, 'Rockstar', 'Accion/Aventura', 2013,'Es un videojuego de acción-aventura de mundo abierto desarrollado por el estudio Rockstar North y distribuido por Rockstar Games.', 1);
@@ -55,10 +55,14 @@ INSERT INTO juego VALUES (0, 'Rocket League', 100.00, 'Psyonix', 'Futbol/Deporte
 INSERT INTO juego VALUES (0, 'FIFA 22', 1400.00, 'Electronic Arts', 'Futbol/Deportes', 2021,'FIFA es una saga de videojuegos de fútbol publicados anualmente por Electronic Arts bajo el sello de EA Sports creado en Japón Cuando la saga comenzó a finales de 1993 se destacó por ser el primero en tener una licencia oficial de la FIFA', 1);
 INSERT INTO juego VALUES (0, 'NBA 2K22', 400.00, '2K Games', 'Baloncesto/Deportes', 2021, 'NBA 2K22 es un videojuego de simulación de baloncesto de 2021 desarrollado por Visual Concepts y publicado por 2K Sports, basado en la National Basketball Association. Es la entrega número 23 de la franquicia NBA 2K y el sucesor de NBA 2K21.', 1);
 INSERT INTO juego VALUES (0, 'FIFA 21', 1300.00, 'Electronic Arts', 'Futbol/Deportes', 2020,'FIFA 21 es un videojuego de fútbol del año 2020 disponible para Microsoft Windows, PlayStation 4, Xbox One y Nintendo Switch el 9 de octubre de 2020, y también es el primer videojuego de la serie FIFA para PlayStation 5 y Xbox Series X|S.', 1);
-INSERT INTO juego VALUES (0, 'Madden NFL 22', 1400.00, 'Electronic Arts', 'Futbol/Deportes', 2021,'Madden NFL 22 es un videojuego de fútbol americano basado en la National Football League, desarrollado por EA Tiburon y publicado por Electronic Arts.', 1);
+INSERT INTO juego VALUES (0, 'Madden NFL 22', 1400.00, 'Electronic Arts', 'Futbol/Deportes', 2021,'Madden NFL 22 es un videojuego de fútbol americano basado en la National Football League, desarrollado por EA Tiburon y publicado por Electronic Arts. Es una entrega de la larga serie Madden NFL', 1);
 
+
+##update usuario set passwordUsuario = "ef8eb12674e8e1358266c00f9380fe1be78c07dc" where correoUsuario ='admin';
+
+SELECT * FROM venta;
+SELECT * FROM detalleVenta;
 
 SELECT * FROM usuario;
 SELECT * FROM juego;
-SELECT * from usuario WHERE (correoUsuario='b' && passwordUsuario = 'password');
-
+SELECT * FROM venta;
