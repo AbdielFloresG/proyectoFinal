f8b95afeb46594cdb9189994cb8714641e4401a0
Abdiel Flores <abdielfg@gmail.com>
2022-06-14 17:16:21 -0600

    actualizacion de base de datos

--- database/gameStore.sql
+++ database/gameStore.sql
@@ -1,4 +1,3 @@
-drop database gamestore;
 DROP DATABASE gameStore;
 CREATE DATABASE gameStore;
 USE gameStore;
@@ -8,10 +7,7 @@ idCambio INT PRIMARY KEY AUTO_iNCREMENT,
 fechaModificacion DATETIME NOT NULL, 
 descripcionCambio VARCHAR(500)
 );
-select*from usuario;
-drop table usuario;
-INSERT INTO `gamestore`.`usuario` (`nombreUsuario`, `apellidoUsuario`, `correoUsuario`, `passwordUsuario`,`rolUsuario`) VALUES ('Abdiel', 'Flores', 'admin', 'ef8eb12674e8e1358266c00f9380fe1be78c07dc','admin');
-INSERT INTO `gamestore`.`usuario` (`nombreUsuario`, `apellidoUsuario`, `correoUsuario`, `passwordUsuario`,`rolUsuario`) VALUES ('Abdiel', 'Flores', 'a', 'ef8eb12674e8e1358266c00f9380fe1be78c07dc','user');
+
 CREATE TABLE Usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT, 
 nombreUsuario VARCHAR(50) NOT NULL,
@@ -35,10 +31,11 @@ activo INT NOT NULL
 CREATE TABLE Venta(
 idVenta INT PRIMARY KEY AUTO_INCREMENT,
 idUsuario INT, FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario) ON UPDATE CASCADE ON DELETE CASCADE,
+correoUsuario VARCHAR(50), FOREIGN KEY(correoUsuario) REFERENCES Usuario(correoUsuario) ON UPDATE CASCADE ON DELETE CASCADE,
 fechaVenta DATETIME,
 monto FLOAT NOT NULL
 );
-insert into venta values(0,2,now(),2800);
+
 CREATE TABLE detalleVenta(
 	idTrasaccion INT PRIMARY KEY AUTO_INCREMENT,
     idVenta INT, FOREIGN KEY(idVenta) REFERENCES venta(idVenta) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -46,27 +43,6 @@ CREATE TABLE detalleVenta(
     nombreJuego varchar(200),
     cantidad float
 );
-
-
-INSERT INTO juego VALUES (0, 'Rainbow Six Siege', 200.00, 'Ubisoft', 'Shooter', 2015,'Videojuego de disparos táctico en línea desarrollado por Ubisoft Montreal y distribuidor por Ubisoft.', 1);
-INSERT INTO juego VALUES (0, 'GTA V', 300.00, 'Rockstar', 'Accion/Aventura', 2013,'Es un videojuego de acción-aventura de mundo abierto desarrollado por el estudio Rockstar North y distribuido por Rockstar Games.', 1);
-INSERT INTO juego VALUES (0, 'Rocket League', 100.00, 'Psyonix', 'Futbol/Deportes', 2015,'Es un videojuego que combina el fútbol con los vehículos. Fue desarrollado por Psyonix y lanzado el 7 de julio de 2015.', 1);
-INSERT INTO juego VALUES (0, 'FIFA 22', 1400.00, 'Electronic Arts', 'Futbol/Deportes', 2021,'FIFA es una saga de videojuegos de fútbol publicados anualmente por Electronic Arts bajo el sello de EA Sports creado en Japón Cuando la saga comenzó a finales de 1993 se destacó por ser el primero en tener una licencia oficial de la FIFA', 1);
-INSERT INTO juego VALUES (0, 'NBA 2K22', 400.00, '2K Games', 'Baloncesto/Deportes', 2021, 'NBA 2K22 es un videojuego de simulación de baloncesto de 2021 desarrollado por Visual Concepts y publicado por 2K Sports, basado en la National Basketball Association. Es la entrega número 23 de la franquicia NBA 2K y el sucesor de NBA 2K21.', 1);
-INSERT INTO juego VALUES (0, 'FIFA 21', 1300.00, 'Electronic Arts', 'Futbol/Deportes', 2020,'FIFA 21 es un videojuego de fútbol del año 2020 disponible para Microsoft Windows, PlayStation 4, Xbox One y Nintendo Switch el 9 de octubre de 2020, y también es el primer videojuego de la serie FIFA para PlayStation 5 y Xbox Series X|S.', 1);
-INSERT INTO juego VALUES (0, 'Madden NFL 22', 1400.00, 'Electronic Arts', 'Futbol/Deportes', 2021,'Madden NFL 22 es un videojuego de fútbol americano basado en la National Football League, desarrollado por EA Tiburon y publicado por Electronic Arts. Es una entrega de la larga serie Madden NFL', 1);
-
-
-##update usuario set passwordUsuario = "ef8eb12674e8e1358266c00f9380fe1be78c07dc" where correoUsuario ='admin';
-
-SELECT monto FROM venta;
-SELECT sum(cantidad) FROM detalleVenta where idVenta=1 ;
-
-SELECT idVenta FROM venta GROUP BY idVenta=1;
-
-SELECT * FROM usuario;
-SELECT * FROM juego;
-SELECT * FROM detalleVenta;
 
 CREATE TRIGGER insercionUsuario AFTER INSERT ON Usuario
 FOR EACH ROW
@@ -133,3 +109,25 @@ CREATE TRIGGER actualizarDetalleVenta AFTER UPDATE ON detalleVenta
 FOR EACH ROW
 INSERT INTO Log_
 VALUES(0,CURRENT_TIMESTAMP(),CONCAT('Se realizo una actualizar en la tabla detalleVenta'));
+
+INSERT INTO gamestore.usuario (nombreUsuario, apellidoUsuario, correoUsuario, passwordUsuario,rolUsuario) VALUES ('Abdiel', 'Flores', 'admin', '9d3b8487f1fcc17d70c4af51077db384fa2834fb','admin');
+INSERT INTO `gamestore`.`usuario` (`nombreUsuario`, `apellidoUsuario`, `correoUsuario`, `passwordUsuario`,`rolUsuario`) VALUES ('Abdiel', 'Flores', 'a', '123456','user');
+
+INSERT INTO juego VALUES (0, 'Rainbow Six Siege', 200.00, 'Ubisoft', 'Shooter', 2015,'Videojuego de disparos táctico en línea desarrollado por Ubisoft Montreal y distribuidor por Ubisoft.', 1);
+INSERT INTO juego VALUES (0, 'GTA V', 300.00, 'Rockstar', 'Accion/Aventura', 2013,'Es un videojuego de acción-aventura de mundo abierto desarrollado por el estudio Rockstar North y distribuido por Rockstar Games.', 1);
+INSERT INTO juego VALUES (0, 'Rocket League', 100.00, 'Psyonix', 'Futbol/Deportes', 2015,'Es un videojuego que combina el fútbol con los vehículos. Fue desarrollado por Psyonix y lanzado el 7 de julio de 2015.', 1);
+INSERT INTO juego VALUES (0, 'FIFA 22', 1400.00, 'Electronic Arts', 'Futbol/Deportes', 2021,'FIFA es una saga de videojuegos de fútbol publicados anualmente por Electronic Arts bajo el sello de EA Sports creado en Japón Cuando la saga comenzó a finales de 1993 se destacó por ser el primero en tener una licencia oficial de la FIFA', 1);
+INSERT INTO juego VALUES (0, 'NBA 2K22', 400.00, '2K Games', 'Baloncesto/Deportes', 2021, 'NBA 2K22 es un videojuego de simulación de baloncesto de 2021 desarrollado por Visual Concepts y publicado por 2K Sports, basado en la National Basketball Association. Es la entrega número 23 de la franquicia NBA 2K y el sucesor de NBA 2K21.', 1);
+INSERT INTO juego VALUES (0, 'FIFA 21', 1300.00, 'Electronic Arts', 'Futbol/Deportes', 2020,'FIFA 21 es un videojuego de fútbol del año 2020 disponible para Microsoft Windows, PlayStation 4, Xbox One y Nintendo Switch el 9 de octubre de 2020, y también es el primer videojuego de la serie FIFA para PlayStation 5 y Xbox Series X|S.', 1);
+INSERT INTO juego VALUES (0, 'Madden NFL 22', 1400.00, 'Electronic Arts', 'Futbol/Deportes', 2021,'Madden NFL 22 es un videojuego de fútbol americano basado en la National Football League, desarrollado por EA Tiburon y publicado por Electronic Arts. Es una entrega de la larga serie Madden NFL', 1);
+
+
+##update usuario set passwordUsuario = "ef8eb12674e8e1358266c00f9380fe1be78c07dc" where correoUsuario ='a';
+##update usuario set passwordUsuario = "9d3b8487f1fcc17d70c4af51077db384fa2834fb" where correoUsuario ='a';
+
+SELECT * FROM venta;
+SELECT * FROM detalleVenta;
+
+SELECT * FROM usuario;
+SELECT * FROM juego;
+SELECT * FROM venta;
